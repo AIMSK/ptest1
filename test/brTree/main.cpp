5624540545bef1105a715f0586e3631ef6754fc8
Your Name <you@example.com>
2018-10-11 20:12:22 -0700

    1

--- test/brTree/main.cpp
+++ test/brTree/main.cpp
@@ -5,7 +5,9 @@ int main(int argc, char ** argv)
 {
     //brNode * tmp;
     //tmp = (brNode *) malloc(sizeof(brNode));
+#ifdef DEBUG
     printf("main test \n");
+#endif
     brNodeFunc brt;
     brNodeFunc *pbrt = new brNodeFunc();
     delete pbrt;
