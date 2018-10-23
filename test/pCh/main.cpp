74119f73033cea55141883a4346eeeaa0b93641e
Your Name <you@example.com>
2018-10-22 22:54:54 -0700

    1

--- test/pCh/main.cpp
+++ test/pCh/main.cpp
@@ -5,8 +5,10 @@ int main (int argc, char ** argv)
 {
     wchar_t * chstr = L"测试";
     char chr;
+    int n;
     unsigned int * p =  (unsigned int *) chstr;
-    //printf ("0x%x\n",p[0]);
+    n = (int)p[0];
+    printf ("0x%x\n",p[0]);
     FILE * fr; 
     fr = fopen("piny.dat","rb");
     if(fr == NULL)
@@ -17,21 +19,24 @@ int main (int argc, char ** argv)
     //fseek( fr, 3, 0);
     //fread( &chr, 1, 1, fr);
     //printf("%c \n", chr);
+    fseek( fr, (n-0x4e00), 0);
+    fread( &chr, 1, 1, fr);
+    printf("offset 测: %c \n", chr);
 
     fseek( fr, 3, 0);
     fread( &chr, 1, 1, fr);
-    printf("%c \n", chr);
+    printf("offset 3: %c \n", chr);
 
     fseek( fr, 0x100, 0);
     fread( &chr, 1, 1, fr);
-    printf("%c \n", chr);
+    printf("offset 0x100:%c \n", chr);
 
     fseek( fr, 0x51a4, 0);
     fread( &chr, 1, 1, fr);
-    printf("%c \n", chr);
+    printf("offset 0x51a4:%c \n", chr);
 
     fseek( fr, 0x51a5, 0);
     fread( &chr, 1, 1, fr);
-    printf("%c \n", chr);
+    printf("offset 0x51a5:%c \n", chr);
     return 0;
 }
